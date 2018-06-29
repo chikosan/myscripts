daa8e98de450806e840c69e94487ef411871a644
shai chikorel <chikosan@gmail.com>
2018-06-29 22:41:49 +0300

    v1.0

---
+++ ipupdate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Update your home ip with digitalocean.com dns using APIv2
+#ShaiChikorel
+
+digiapi=$(cat digiapik)
+homeip=$(curl -s ipinfo.io | jq -r '.ip')
+remoteip=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $digiapi" "https://api.digitalocean.com/v2/domains/chikorel.com/records/48211228/" | jq -r '.domain_record.data')
+
+# check if jq install and if not install it
+if [ ! -f /usr/bin/jq ]; then
+	yum install jq -y 1>2 
+	echo " jq was installed on your system"
+fi
+
+# compare localip with server ip
+if [ $homeip != $remoteip ]; then
+	curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $digiapi" -d '{"data":"'"$homeip"'"}' "https://api.digitalocean.com/v2/domains/chikorel.com/records/48211228"
+	echo " your localIP was update DNS myip.chikorel.com\n"
+fi
+
+
+
+
+echo -e "\nyour ip is $homeip and the dns ip is $remoteip"
+
