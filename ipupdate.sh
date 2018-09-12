d60e370db44665249c7ccd41d26b5831f68361e3
Shai Chikorel <chikosan@gmail.com>
2018-09-12 23:10:10 +0300

    Add functiona

--- ipupdate.sh
+++ ipupdate.sh
@@ -3,15 +3,16 @@
 #Update your ip with digitalocean.com dns using APIv2
 
 
+
+function main() 
+{
+
+## Vars
+
 digiapi=$(cat digiapik)
 homeip=$(curl -s ipinfo.io | jq -r '.ip')
 remoteip=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $digiapi" "https://api.digitalocean.com/v2/domains/chikorel.com/records/48211228/" | jq -r '.domain_record.data')
 
-# check if jq install and if not install it
-if [ ! -f /usr/bin/jq ]; then
-	yum install jq -y 1>2 
-	echo " jq was installed on your system"
-fi
 
 # compare localip with server ip
 if [ $homeip != $remoteip ]; then
@@ -19,8 +20,26 @@ if [ $homeip != $remoteip ]; then
 	echo " your localIP was update DNS myip.chikorel.com\n"
 fi
 
-
-
-
 # echo -e "\nyour ip is $homeip and the dns ip is $remoteip"
+}
+
+function errorcheck() 
+{
+
+# check if jq install and if not install it
+if [ ! -f /usr/bin/jq ]; then
+        yum install jq -y 1>2
+        echo " jq was installed on your system"
+fi
+
+# if digiapik not found
+if [ ! -f digiapik ]; then
+ echo "digiapik not found"
+ exit 1
+fi
+
+}
+
+errorcheck
+main
 
