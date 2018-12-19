d0877ee1805007e6e72bd23e5cab330230c684ce
Shai <Chikosan@gmail.com>
2018-12-19 20:33:14 +0200

    Create git.centos7.sh

---
+++ git.centos7.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+# Install git on centos7
+
+sudo cat <<EOT >> /etc/yum.repos.d/wandisco-git.repo
+[wandisco-git]
+name=Wandisco GIT Repository
+baseurl=http://opensource.wandisco.com/centos/7/git/$basearch/
+enabled=1
+gpgcheck=1
+gpgkey=http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
+EOT
+sudo rpm --import http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
+sudo yum install git -y
